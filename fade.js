eb994455f779d29facf06ece167b47d37a3c9c53
Paige Lee <paigelee@Paiges-MacBook-2.local>
2020-12-23 11:22:36 -0500

    favicon and stuff

--- fade.js
+++ fade.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('#footer').text('designed by paige lee, updated 12-23-20');
+
+    $('head').append('<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"><link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"><link rel="manifest" href="/site.webmanifest"><meta name="msapplication-TileColor" content="#da532c"><meta name="theme-color" content="#ffffff"></meta>');
+    
     window.addEventListener('resize', function() {
     if ($(window).width() < 700) {
         $('#stackednavbar').show();
@@ -8,6 +12,22 @@ $(document).ready(function () {
         $("#navbar").show()
     }
     })
+    cardhover(1);
+    cardhover(2);
+    cardhover(3);
+    cardhover(4);
+    cardhover(5);
+    cardhover(6);
+
+    function cardhover(i) {
+        $("#card" + i).mouseover(function() {
+            $('#card'+ i + ' .card-box').show();
+        });
+        $("#card" + i).mouseout(function() {
+            $('#card'+ i + ' .card-box').hide();
+        });
+    };
+
     $("#main").fadeIn(500);
     $('#main2').fadeIn(1500);
     $('#card1').fadeIn(1000);
